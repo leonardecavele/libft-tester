90d691772cc02f7b819ddef6cbaa27c6eb94cdca
leonardecavele <leonard.decavele@gmail.com>
2025-11-09 20:05:03 +0100

    removed stdint type

--- tests/ft_strnstr.c
+++ tests/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:17:09 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/09 18:33:22 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/09 20:04:52 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ static int	strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (!n)
 		return (0);
-	while (s1[i] && i + 1 < n && (uint8_t)s1[i] == (uint8_t)s2[i])
+	while (s1[i] && i + 1 < n && (unsigned char)s1[i] == (unsigned char)s2[i])
 		i++;
-	return ((uint8_t)s1[i] - (uint8_t)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 char	*strnstr(const char *haystack, const char *needle, size_t len)
